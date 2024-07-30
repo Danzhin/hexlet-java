ced2e8995e4787727a40eef398414898cd9a116f
danzhin <dronzhin123@gmail.com>
2024-07-30 18:04:01 +0300

    first commit

---
+++ io/hexlet/One.java
@@ -0,0 +1,10 @@
+package io.hexlet;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class One {
+    public static void main(String[] args) {
+      var ReverseWord = StringUtils.reverse("Hexlet");
+      System.out.println(ReverseWord);
+    }
+}
